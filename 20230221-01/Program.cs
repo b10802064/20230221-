4770449bc1b7a98f80761213bd1ac2e43f66410b
kuuhaku <b10802064@g.chu.edu.tw>
2023-02-21 11:35:42 +0800

    加入專案檔案。

---
+++ 20230221-01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230221_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Class1> list = new List<Class1>();
+            Console.Write("輸入數字串(以逗點分割)");
+            string[] arr = Console.ReadLine().Split(',');
+            int count = 0;
+            foreach (var item in arr)
+            {
+               list.Add(new Class1() { count = count,data = item});
+                count++;
+            }
+            Console.Write("輸出:");
+            var result = list.OrderByDescending(x => x.count);
+            foreach (var item in result)
+            {
+                if (item.count == 0)
+                {
+                    Console.WriteLine(item.data );
+                }
+                else
+                {
+                    Console.Write(item.data + ",");
+                }
+                
+
+            }
+
+        }
+    }
+}
---
+++ 20230221-02/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230221_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("輸入數字串(以逗點分割):");
+            string[] arr = Console.ReadLine().Split(',');
+            var even = arr.Where(x=>Convert.ToDouble(x)%2 == 0).OrderBy(x=> Convert.ToDouble(x));
+            var odd = arr.Where(x => Convert.ToDouble(x) % 2 != 0).OrderBy(x => Convert.ToDouble(x));
+
+            int count = 0;
+            int count1 = 0;
+            Console.Write("奇數:");
+            foreach (var x in odd)
+            {
+                if (count1 < odd.Count() - 1)
+                {
+                    Console.Write(x + ",");
+                    count1++;
+                }
+                else
+                {
+                    Console.WriteLine(x);
+                }
+            }
+
+
+
+            Console.Write("偶數:");
+            foreach(var x in even)
+            {
+                if(count< even.Count()-1)
+                {
+                    Console.Write(x+",");
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine(x);
+                }
+            }
+
+
+
+
+
+
+        }
+    }
+}
